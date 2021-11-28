a06f9832b25898c31c805fcd939355aaff6d02d4
yklee7285 <dariuslee7285@gmail.com>
2021-11-28 09:21:55 +0000

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+import Navbar from '$lib/Navbar.svelte';
+</script>
+
+<Navbar />
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Greeting from '$lib/Greeting.svelte';
+    import Entry from '$lib/Entry.svelte';
+    import EntryModal from '$lib/EntryModal.svelte';
+</script>
+
+<Greeting />
+<!-- Entries -->
+<section class="container px-4 py-3">
+    <div class="d-flex justify-content-between">
+   	 <div class="p-2">Mood Log</div>
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+    </div>
+
+    <div class="list-group mb-3">
+   	 <!-- Individual Entries -->
+    	<Entry />
+        <EntryModal />
+   	 
+    </div>
+</section>
+
